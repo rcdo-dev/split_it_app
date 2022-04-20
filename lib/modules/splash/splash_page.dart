bf3ec31340229d05cae39927db601897425502e6
rcdo-dev <rcdo.dev@gmail.com>
2022-04-20 11:54:12 -0300

    feat: splash page updated with rectangles in the details.

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -9,16 +9,78 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.colors.background,
       body: Container(
         decoration: BoxDecoration(
           gradient: AppTheme.gradients.background,
         ),
-        child: Center(
-          child: Image.asset(
-            'assets/images/logo.png',
-            width: 128,
-            height: 112,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/retangulo-dir.png',
+                        width: 198,
+                        height: 98,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/retangulo-dir.png',
+                        width: 114,
+                        height: 54,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                width: 128,
+                height: 112,
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/retangulo-esq.png',
+                        width: 114,
+                        height: 54,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/retangulo-esq.png',
+                        width: 198,
+                        height: 98,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
