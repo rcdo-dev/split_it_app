ea346a1b44477086918881232dafb74f2a08fd1d
rcdo-dev <rcdo.dev@gmail.com>
2022-04-25 12:05:24 -0300

    refactor: changed font title.

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -15,18 +17,22 @@ class _LoginPageState extends State<LoginPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Row(
-            children: [
-              SizedBox(
-                width: 236.0,
-                child: Text(
-                  'Divida suas contas com seus amigos',
-                  style: GoogleFonts.breeSerif(
-                    fontSize: 40.0,
+          Padding(
+            padding: const EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 236.0,
+                  child: Text(
+                    'Divida suas contas com seus amigos',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           Column(
             children: <Widget>[
@@ -38,6 +44,7 @@ class _LoginPageState extends State<LoginPage> {
                   'Faça seu login com uma das contas abaio',
                   style: GoogleFonts.inter(
                     fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ),
@@ -53,6 +60,7 @@ class _LoginPageState extends State<LoginPage> {
                   'Entrar com Google',
                   style: GoogleFonts.inter(
                     fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ),
@@ -68,6 +76,7 @@ class _LoginPageState extends State<LoginPage> {
                   'Entrar com Apple',
                   style: GoogleFonts.inter(
                     fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ),
